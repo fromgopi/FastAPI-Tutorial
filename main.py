fca912f5aecaa9f52477c412917687e50166586a
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-06-22 00:13:54 +0530

    Initial Code checkin

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('main.html', {'request': request})
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port="8000")
